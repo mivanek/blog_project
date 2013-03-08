92e034202116c890e25250e0024889b52b6337a4
Marko Ivanek <markoivanek@gmail.com>
2013-03-08 15:38:36 +0100

    Remove remaining blogpost tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ BlogProject::Application.routes.draw do
 
   resources :articles
 
-  root to: 'articles/index'
+  root to: 'articles#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
